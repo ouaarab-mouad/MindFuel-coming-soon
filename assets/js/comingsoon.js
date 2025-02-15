6662307c3564e14f42b4b970f383948ff9fb0cf6
mouad <warabmoad@gmail.com>
2025-02-15 23:16:45 +0100

    remove some img and change text color

--- assets/js/comingsoon.js
+++ assets/js/comingsoon.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("Dec 25, 2022 00:00:00").getTime();
+    const countDate = new Date("mar 25, 2025 00:00:00").getTime();
 
     const currentTime = new Date().getTime();
     console.log(currentTime);
